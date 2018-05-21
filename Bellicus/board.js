1b4f66e94aa6f9b9abbca307980038fa60339341
VANDAELE Eymeric <eymeric.vandaele@soprasteria.com>
2018-05-21 21:35:24 +0200

    starting bellicus project

---
+++ Bellicus/board.js
@@ -0,0 +1,39 @@
+function addElement(player, value){
+  var element = "<button class='element p"+player+"' id=button" +value +" draggable='true' ondragstart='drag(event)' ondrop='drop(event)' ondragover='allowDrop(event)'>"+value+"</button>"
+  return element
+}
+function createBoard(){
+  var finalBoard = "<div id='drag'>test</div><table>";
+  var index = 0;
+  var joueur = 1;
+  finalBoard += "<tr>";
+  for (colonne = 0; colonne < 5 ; colonne++){
+    if(colonne == 2){
+      finalBoard += "<td colspan='2' id="+index+">"+addElement(joueur,index)+"</td>";
+    }else{
+      finalBoard += "<td id="+index+">"+addElement(joueur,index)+"</td>";
+    }
+    index++;
+  }
+  finalBoard += "</tr>";
+  for (ligne = 0; ligne < 6 ; ligne++){
+    finalBoard += "<tr>";
+    for (colonne = 0; colonne < 6 ; colonne++){
+      finalBoard += "<td id="+index+">"+addElement(joueur,index)+"</td>";
+      index++;
+    }
+    finalBoard += "</tr>";
+  }
+  finalBoard += "<tr>";
+  for (colonne = 0; colonne < 5 ; colonne++){
+    if(colonne == 2){
+      finalBoard += "<td colspan='2' id="+index+">"+addElement(joueur,index)+"</td>";
+    }else{
+      finalBoard += "<td id="+index+">"+addElement(joueur,index)+"</td>";
+    }
+    index++;
+  }
+  finalBoard += "</tr>";
+  finalBoard += "</table>";
+  document.getElementById("board").innerHTML = finalBoard;
+}
---
+++ Bellicus/drag.js
@@ -0,0 +1,58 @@
+var memory;
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    memory = ev.path[1];
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    cible = ev.target;
+    console.log(memory.children[0]);
+    console.log(cible);
+
+    exchangeElements(memory.children[0], cible);
+}
+
+
+// Note: Cloned copy of element1 will be returned to get a new reference back
+function exchangeElements(element1, element2)
+{
+    var clonedElement1 = element1.cloneNode(true);
+    var clonedElement2 = element2.cloneNode(true);
+
+    element2.parentNode.replaceChild(clonedElement1, element2);
+    element1.parentNode.replaceChild(clonedElement2, element1);
+    console.log(element2)
+    console.log(element1)
+
+    return clonedElement1;
+}
+
+function swapNodes(n1, n2) {
+    var p1 = n1.parentNode.children[0];
+    var p2 = n2.parentNode.children[0];
+    var i1, i2;
+
+    if ( !p1 || !p2 || p1.isEqualNode(n2) || p2.isEqualNode(n1) ) return;
+    for (var i = 0; i < p1.children.length; i++) {
+        if (p1.children[i].isEqualNode(n1)) {
+            i1 = i;
+        }
+    }
+    for (var i = 0; i < p2.children.length; i++) {
+        if (p2.children[i].isEqualNode(n2)) {
+            i2 = i;
+        }
+    }
+    if ( p1.isEqualNode(p2) && i1 < i2 ) {
+        i2++;
+    }
+    console.log(p1);
+    p1.insertBefore(n2, p1.children[i1]);
+    p2.insertBefore(n1, p2.children[i2]);
+}
